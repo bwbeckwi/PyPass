8a0ba5ce61e6ce9fc3c156e05ebae186a4ddbd7e
mjbaz90 <mjbaz90@gmail.com>
2018-04-09 17:17:07 -0700

    Adding changes to generate multiple passwords

--- password_generator.py
+++ password_generator.py
@@ -7,22 +7,40 @@ import secrets
 
 @click.command()
 @click.argument('int_length', type=int)
-def run(int_length):
+@click.option('--int_num_pw', default=1, type=int, help='Number of passwords to be generated.')
+def run(int_length, int_num_pw):
     """Generates a random password using Python 3.6's secrets library, runs it through the SHA1 hashing algorithm, and checks it against
-    haveibeenpwned.com's password range API. If the password is good, it conveniently copies it to the clipboard, if not, it runs recursively
-    until it finds a good password (one that has not been found during any known data breaches."""
-    password = generate_password(int_length)
-    hashed_password = generate_hash(password)
-    check_hash(password, hashed_password)
-
+    haveibeenpwned.com's password range API. If the password is good, it conveniently copies it to the clipboard (for single passwords),
+    else it will print the list of paswords. If the password fails the test, a new one will be generated and returned."""
+    password_list = []
+    if int_num_pw == 1:
+        password = generate_password(int_length)
+        hashed_password = generate_hash(password)
+        check_hash(password, hashed_password, int_length, int_num_pw)
+        pyperclip.copy(password)
+        print('Copied password to clipboard.')
+    elif int_num_pw > 1:
+        while int_num_pw > 0:
+            password = generate_password(int_length)
+            hashed_password = generate_hash(password)
+            good_password = check_hash(password, hashed_password, int_length, int_num_pw)
+            password_list.append(good_password)
+            int_num_pw -= 1
+        print('\n\nPasswords are as follows:\n')
+        for pw in password_list:
+            print(pw)
+    elif int_num_pw <= 0:
+        return 'Error, number of passwords to be generated must be greater than zero.'
 
 
 def generate_password(int_length):
+    print('Generating password...')
     return (''.join(secrets.choice(
         'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890`~!@#$%^&*()-=+_:"/.,<>[]\{\}\\\'') for i in range(int_length)))
 
 
 def generate_hash(str_password):
+    print('Hashing password...')
     str_password = str_password.encode('utf-8')
     sha1_obj = hashlib.sha1(str_password)
     hashed_password = sha1_obj.hexdigest()
@@ -30,21 +48,25 @@ def generate_hash(str_password):
     return hashed_password
 
 
-def check_hash(password, hashed_password):
+def check_hash(password, hashed_password, int_length, int_num_pw):
+    print('Checking hash against the pwned passwords API...')
     first_five = hashed_password[0:5]
     remaining = hashed_password[5:]
     r = requests.get('https://api.pwnedpasswords.com/range/{}'.format(first_five))
-
     r_list = r.text.split('\r\n')
     hash_list = []
     for item in r_list:
         current_hash  = item.split(':')[0]
         if remaining in current_hash:
             print('Password has been hacked, generating new password.')
-            run()
-    pyperclip.copy(password)
-    print('Found a good password, copied to clipboard.')
-
+            password = generate_password(int_length)
+            hashed_password = generate_hash(password)
+            good_password = check_hash(password, hashed_password, int_length, int_num_pw)
+            return good_password
+        else:
+            print('Password is good.')
+            return password
 
 if __name__ == '__main__':
         run()
+
